838083a80b1b33d427a16a570fbdee119a149790
xiaYuTian11 <38251546+xiaYuTian11@users.noreply.github.com>
2021-06-24 17:36:04 +0800

    添加忽略文件

---
+++ tanmw/test/com/tanmw/Test01.java
@@ -0,0 +1,13 @@
+package com.tanmw;
+
+/**
+ * @author TMW
+ * @since 2021/6/24 17:32
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        final String property = System.getProperty("user.dir");
+        System.out.println(property);
+    }
+}
